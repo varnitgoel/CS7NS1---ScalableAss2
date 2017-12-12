358320da35e4849b1f0769fd563791665fd5c8eb
Varnit Goel <17356399+varnitgoel@users.noreply.github.com>
2017-12-12 18:52:31 +0000

    Add files via upload
    Define WorkerClass

--- Worker.py
+++ Worker.py
@@ -5,13 +5,23 @@ Created on Tue Dec 10 17:41:59 2017
 @author: Varnit Goel
 """
 
-import json
+import json, requests, subprocess 
 
+def workerclass():
+    IP = input("Enter IP of the Controller -  ")
+    controllerPort = input("Enter the port of the Controller -  ")
 
+    p = requests.get("http://{}:{}/repo".format(IP, controllerPort), json={'status_of_pull': False}) 
+    json_data = json.loads(p.text)
+    
+    ##Link of repo.
+    link = json_data['repo']
+    subprocess.call(["bash", "workerInitScript.sh", link])  
 
+    p = requests.get("http://{}:{}/repo".format(IP, controllerPort), json={'status_of_pull': True})
 
 
 
 
 if __name__ == "__main__":
-    run()
+    workerclass()
