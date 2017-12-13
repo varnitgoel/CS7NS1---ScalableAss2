9574e1083527aeb83f9d3ade0076ed4719702c58
Varnit Goel <17356399+varnitgoel@users.noreply.github.com>
2017-12-13 12:08:38 +0000

    Worker class updates
    Still with rolling back my cloned repo to acquire sha and then calculating to return the complexity

--- Worker.py
+++ Worker.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Dec 10 17:41:59 2017
+Created on Tue Dec 11 10:36:00 2017
 
 @author: Varnit Goel
 """
@@ -8,18 +8,27 @@ Created on Tue Dec 10 17:41:59 2017
 import json, requests, subprocess 
 
 def workerclass():
-    IP = input("Enter IP of the Controller -  ")
+    controllerIP = input("Enter IP of the Controller -  ")
     controllerPort = input("Enter the port of the Controller -  ")
+    no_of_commits = 0
 
-    p = requests.get("http://{}:{}/repo".format(IP, controllerPort), json={'status_of_pull': False}) 
+    p = requests.get("http://{}:{}/repo".format(controllerIP, controllerPort), json={'status_of_pull': False}) 
     json_data = json.loads(p.text)
     
     ##Link of repo.
     link = json_data['repo']
-    subprocess.call(["bash", "workerInitScript.sh", link])  
+    subprocess.call(["bash", "workerInitScript.sh", link])   
 
-    p = requests.get("http://{}:{}/repo".format(IP, controllerPort), json={'status_of_pull': True})
-
+    p = requests.get("http://{}:{}/repo".format(controllerIP, controllerPort), json={'status_of_pull': True})
+    
+    
+    ##check Commits
+    ifCommits = True
+    while ifCommits:
+        r = requests.get("http://{}:{}/cyclomatic".format(controllerIP, controllerPort))
+        json_data = json.loads(p.text)
+        print(json_data)
+        print("Received: {}".format(json_data['sha']))
 
 
 
