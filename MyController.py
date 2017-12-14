9988a03473883684d146bc58a5ef7dfa6336ac77
Varnit Goel <17356399+varnitgoel@users.noreply.github.com>
2017-12-14 00:54:47 +0000

    Add files via upload

--- MyController.py
+++ MyController.py
@@ -13,7 +13,7 @@ import time, getpass
 appl = Flask(__name__)
 api = Api(appl)
 
-class controller():
+class controller(): ##Server Class
     def __init__(self):
         self.no_of_workers = input ("Number of workers needed?: ")
         self.nu_of_workers = int (self.no_of_workers)
@@ -21,7 +21,35 @@ class controller():
         self.current_workers = 0 ##WORKERS CONNECTED TO THE MANAGER
         #Timer Starts 
         self.startTime = 0.0 
+    
+#*************************************************************************************
+    
+        #Request repo. info
+        g_username = input("Enter Github username or press enter to proceed:")
+        print(len(g_username))
+        if len(g_username) != 0:
+            g_password = getpass.getpass("Enter Password to proceed: ")
+        page = True  #check if more pages on github 
+        page_no = 1  # Current page of github 
+        self.commitlist = []  # List containing all commit sha values
+        while Page:
+            if len(g_username) == 0:
+                p = requests.get("https://api.github.com/repos/varnitgoel/CS7NS1---ScalableAss1/commits?page={}&per_page=100".format(page_no))
+            else:
+                p = requests.get(("https://api.github.com/repos/varnitgoel/CS7NS1---ScalableAss1/commits?page={}&per_page=100".format(page_no), auth=(g_username, g_password))
+            json_data = json.loads(p.text)
+            if len(json_data) < 2:
+                page = False
+            else:
+                for x in json_data:
+                    self.commitlist.append(x['sha'])
+                    print("Commit Sha: {}".format(x['sha']))
+                page_no = page_no + 1
+        self.T_no_of_commits = len(self.commitlist)  #total no. of commits in repos.
+        self.listOfCCs = []
+        print("Total Commits: {}".format(self.T_no_of_commits))
         
+#*************************************************************************************
     
     def get(self):
         argu = self.reqparser.parse_args()
@@ -38,8 +66,10 @@ class controller():
 
 api.add_resource(getRepository, "/repo", endpoint="repo")
 
+#*************************************************************************************
+
 #  Obtaining commits and posting cyclomatic results
-class repo(Resource):
+class CCrepo(Resource):
     def __init__(self):  # Upon initialisation of the class
         global admin
         self.server = admin #when worker wants access
@@ -48,15 +78,13 @@ class repo(Resource):
         #argument for incoming values in JSON
         self.reqparser.add_argument('pullStatus', type = int, location = 'json')
         self.reqparser.add_argument('complexity', type = float, location = 'json') 
+     
+#*************************************************************************************    
+        
 
-        #input git username form user
-        username = input('Enter the username or press enter to continue: ')        
-        #if username is entered, ask for password
-        if len(username) != 0:
-        git_password = getpass.getpass('Enter the Password: ')
-            
-        self.com_List = []  # List of all commited sha values
+
+#*************************************************************************************
         
 if __name__ == "__main__":
     admin = controller()
-    app.run(port=xx)  
+    app.run(port=50000)  
